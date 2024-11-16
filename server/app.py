ed57812775a0d9634e7a0c348a7eb66855494a50
Kyle Greenidge <kylegreen149@gmail.com>
2024-11-15 19:01:10 -0500

    Made view to retreive all players

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 import ipdb
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Player
 
 app = Flask(__name__)
 
@@ -12,6 +12,12 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.get("/players")
+def get_all_players():
+    players = Player.query.all()
+    response_body =[player.to_dict(only=("id", "username")) for player in players]
+    return make_response(response_body, 200)
+    
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
